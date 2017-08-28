fac0ff5062d0943dd7d29faf90eef3a9440b8272
msgctcheng <msgctcheng@gmail.com>
2017-08-27 20:27:52 -0400

    Added all files, mostly done

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log("Keys Loaded.");
+console.log(" ");
+
+exports.twitterKeys = {
+    consumer_key: "2RGC2NqrlRbuv9USrIQ5fcHne",
+    consumer_secret: "IyTBOTHKAZQDEEdgfDvw96v6r8TILiERu5KB1JXEenPyvMFzdK",
+    access_token_key: "890620275770961920-gHwDfbbsblu1KhxnnQ6JjYSRYRPeyDJ",
+    access_token_secret: "gKgyPTb5KRdRINMpRFedgBuzRdeT4t00xDOyJu09H3kue"
+}
+
+exports.spotifyKeys = {
+    id: "3aef42395302403490aede64b16d1936",
+    secret: "645903598ae44fc5bd680a1d339103bb"
+} 
+
---
+++ liri.js
@@ -0,0 +1,89 @@
+//Moduralization
+//"http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece"
+
+//Input
+var inputString = process.argv;
+var operation = inputString[2];
+var searchQ = inputString.slice(3).join("+");
+//Keys
+var apiKeys = require("./keys.js");
+var twitkey = apiKeys.twitterKeys;
+var spotkey = apiKeys.spotifyKeys;
+//OMDB
+var request = require("request");
+//Spotify
+var fs = require("fs");
+var Spotify = require ("node-spotify-api");
+var spotify = new Spotify(spotkey);
+
+//Twitter
+var Twitter = require("twitter");
+var client = new Twitter(twitkey);
+
+ 
+
+if (operation === "my-tweets") {
+    if (searchQ == "") {
+        searchQ = "PandaOfEvil";
+    }
+   client.get('statuses/user_timeline', {screen_name: searchQ}, function(error, tweet, response) {
+        if (!error) {
+           for (var i = 0; i < 20; i++) {
+                console.log("Tweet " + (i + 1) + ":  " + "'" + tweet[i].text + "' - " + tweet[i].created_at);
+                console.log(" ");
+            }  
+        } else {
+            console.log(error);
+        }
+    });
+} 
+else if (operation === "spotify-this-song") {
+    if (searchQ == "") {
+            searchQ = "The Sign";
+    }
+    
+    spotify.search({ type: "track", query: '"'+ searchQ +'"'}).then( function(response) { 
+        console.log("Track:         " + response.tracks.items[0].name);
+        console.log("Artist(s):     " + response.tracks.items[0].album.artists[0].name);
+        console.log("Preview URL:   " + response.tracks.items[0].preview_url);
+        console.log("Album:         " + response.tracks.items[0].album.name);
+        }).catch( function(err) {
+            console.log(err);
+        });
+    
+}
+else if (operation === "movie-this") {
+    if (searchQ == "") {
+        searchQ = "Mr. Nobody";
+    }
+    request("http://www.omdbapi.com/?t="+ searchQ + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
+        if (error) {
+            console.log(error);
+        }
+
+        console.log("Title:        " + JSON.parse(body).Title);
+        console.log(" ");
+        console.log("Year:         " + JSON.parse(body).Year);
+        console.log(" ");
+        console.log("IMDB Rating:  " + JSON.parse(body).imdbRating);
+        console.log(" ");
+        console.log("RT Rating:    " + JSON.parse(body).Ratings[1].Value);
+        console.log(" ");
+        console.log("Country:      " + JSON.parse(body).Country);
+        console.log(" ");
+        console.log("Language:     " + JSON.parse(body).Language);
+        console.log(" ");
+        console.log("Plot:         " + JSON.parse(body).Plot);
+        console.log(" ");
+        console.log("Actors:       " + JSON.parse(body).Actors);
+        console.log(" ");
+    }); 
+}
+else if (operation === "do-what-it-says") {
+    
+}
+else {
+    console.log("Not a valid operation! Please enter a valid operation.");
+}
+
+  
