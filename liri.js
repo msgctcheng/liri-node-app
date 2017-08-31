6a52890b40dd2dc5d4ead02d8290cc1c80708cfa
msgctcheng <msgctcheng@gmail.com>
2017-08-31 12:21:29 -0400

    fixed do-what-it-says

--- liri.js
+++ liri.js
@@ -13,7 +13,7 @@ var spotkey = apiKeys.spotifyKeys;
 var request = require("request");
 //Spotify
 var fs = require("fs");
-var Spotify = require ("node-spotify-api");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(spotkey);
 
 //Twitter
@@ -21,7 +21,10 @@ var Twitter = require("twitter");
 var client = new Twitter(twitkey);
 
 function searchSpot() {
-    spotify.search({ type: "track", query: '"'+ searchQ +'"'}).then( function(response) { 
+    spotify.search({
+        type: "track",
+        query: '"' + searchQ + '"'
+    }).then(function (response) {
         console.log("Track:         " + response.tracks.items[0].name);
         console.log(" ");
         console.log("Artist(s):     " + response.tracks.items[0].album.artists[0].name);
@@ -30,77 +33,77 @@ function searchSpot() {
         console.log(" ");
         console.log("Album:         " + response.tracks.items[0].album.name);
         console.log(" ");
-        }).catch( function(err) {
-            console.log(err);
-        });
-    }
- 
-
-if (operation === "my-tweets") {
-    if (searchQ == "") {
-        searchQ = "PandaOfEvil";
-    }
-   client.get('statuses/user_timeline', {screen_name: searchQ}, function(error, tweet, response) {
-        if (!error) {
-           for (var i = 0; i < 20; i++) {
-                console.log("Tweet " + (i + 1) + ":  " + "'" + tweet[i].text + "' - " + tweet[i].created_at);
-                console.log(" ");
-            }  
-        } else {
-            console.log(error);
-        }
+    }).catch(function (err) {
+        console.log(err);
     });
-} 
-else if (operation === "spotify-this-song") {
-    if (searchQ == "") {
-            searchQ = "The Sign";
-    }
-   
-    searchSpot();
-    
 }
-else if (operation === "movie-this") {
-    if (searchQ == "") {
-        searchQ = "Mr. Nobody";
-    }
-    request("http://www.omdbapi.com/?t="+ searchQ + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
-        if (error) {
-            console.log(error);
+
+runAll();
+
+function runAll() {
+    if (operation === "my-tweets") {
+        if (searchQ == "") {
+            searchQ = "PandaOfEvil";
+        }
+        client.get('statuses/user_timeline', {
+            screen_name: searchQ
+        }, function (error, tweet, response) {
+            if (!error) {
+                for (var i = 0; i < 20; i++) {
+                    console.log("Tweet " + (i + 1) + ":  " + "'" + tweet[i].text + "' - " + tweet[i].created_at);
+                    console.log(" ");
+                }
+            } else {
+                console.log(error);
+            }
+        });
+    } else if (operation === "spotify-this-song") {
+        if (searchQ == "") {
+            searchQ = "The Sign";
         }
 
-        console.log("Title:        " + JSON.parse(body).Title);
-        console.log(" ");
-        console.log("Year:         " + JSON.parse(body).Year);
-        console.log(" ");
-        console.log("IMDB Rating:  " + JSON.parse(body).imdbRating);
-        console.log(" ");
-        console.log("RT Rating:    " + JSON.parse(body).Ratings[1].Value);
-        console.log(" ");
-        console.log("Country:      " + JSON.parse(body).Country);
-        console.log(" ");
-        console.log("Language:     " + JSON.parse(body).Language);
-        console.log(" ");
-        console.log("Plot:         " + JSON.parse(body).Plot);
-        console.log(" ");
-        console.log("Actors:       " + JSON.parse(body).Actors);
-        console.log(" ");
-    }); 
-}
-else if (operation === "do-what-it-says") {
-   
-        fs.readFile("random.txt", "utf8", function (error, data) {
-           searchQ = data.split(",").slice(1).join("+");
-           
-           operation = '"'+ data.split(",").slice(0, 1).join(" ")+'"';
-           
-           searchSpot()
+        searchSpot();
 
-         
-            
+    } else if (operation === "movie-this") {
+        if (searchQ == "") {
+            searchQ = "Mr. Nobody";
+        }
+        request("http://www.omdbapi.com/?t=" + searchQ + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
+            if (error) {
+                console.log(error);
+            }
+
+            console.log("Title:        " + JSON.parse(body).Title);
+            console.log(" ");
+            console.log("Year:         " + JSON.parse(body).Year);
+            console.log(" ");
+            console.log("IMDB Rating:  " + JSON.parse(body).imdbRating);
+            console.log(" ");
+            console.log("RT Rating:    " + JSON.parse(body).Ratings[1].Value);
+            console.log(" ");
+            console.log("Country:      " + JSON.parse(body).Country);
+            console.log(" ");
+            console.log("Language:     " + JSON.parse(body).Language);
+            console.log(" ");
+            console.log("Plot:         " + JSON.parse(body).Plot);
+            console.log(" ");
+            console.log("Actors:       " + JSON.parse(body).Actors);
+            console.log(" ");
         });
-}
-else {
-    console.log("Not a valid operation! Please enter a valid operation.");
-}
+    } else if (operation === "do-what-it-says") {
 
-  
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            var search = data.split(",")[1];
+            searchQ = search.replace(/['"]+/g, '');
+            operation = data.split(",")[0];
+
+
+            runAll();
+
+
+        });
+
+    } else {
+        console.log("Not a valid operation! Please enter a valid operation.");
+    }
+}
